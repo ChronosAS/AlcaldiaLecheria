8ca8a1215f520fa2769fed8352ad282c4028a675
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2024-04-18 13:34:17 -0400

    welcome route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/welcome',function(){
+    return view('welcome');
+})->name('welcome');
 
 Route::middleware([
     'auth:sanctum',
