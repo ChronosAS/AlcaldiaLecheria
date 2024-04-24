f06a7e05b604c4fd30a93f8f1fc8327e5d3824af
leddinlozada223 <leddinlozada@outlook.com>
2024-04-24 08:44:35 -0400

    center

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,23 +7,21 @@
         class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-black/60 bg-fixed">
         <div class="flex h-full items-center justify-center">
             <div class="px-6 text-center text-white md:px-12">
-            <h1 class="mb-6 text-5xl font-bold">Heading</h1>
+            <h1 class="mb-6 text-5xl font-bold ">Heading</h1>
             </div>
         </div>
         </div>
     </div>
 </header>
-<section class="bg-white dark:bg-gray-500 items-center">
-  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-      <div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400 text-center">
-      </div>
-  </div>
-</section>
-  <section class="bg-white dark:bg-gray-500 items-center">
-  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-      <div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400 text-center">
-          <div class="mx-auto">
-              <iframe class=" sm:ml-20 w-full h-[1500px] sm:h-[1300px] block" scrolling="no" src="https://docs.google.com/forms/d/e/1FAIpQLSf8D1hrbKl8u-q1yiWVlEKszUWWd8Wo7uY7fT6OLIxPIpE5sg/viewform?embedded=true" >Cargando…</iframe>
+  <section class="bg-white dark:bg-gray-500">
+  <div class="text-center">
+      <div class=" text-gray-500 sm:text-lg dark:text-gray-400 sm:justify-center">
+            <div class="text-center py-8 space-y-3">
+                <h1 class="section-heading text-uppercase text-[3rem] text-white ">Contactanos</h1>
+                <h2 class="section-subheading text-black text-xl">Nuestra directora de Atención al Ciudadano esta atenta a sus solicitudes.</h2>
+            </div>
+           <div class="mx-auto">
+              <iframe class="  w-full h-[1500px] sm:h-[1300px] block" scrolling="no" src="https://docs.google.com/forms/d/e/1FAIpQLSf8D1hrbKl8u-q1yiWVlEKszUWWd8Wo7uY7fT6OLIxPIpE5sg/viewform?embedded=true" >Cargando…</iframe>
           </div>
       </div>
   </div>
