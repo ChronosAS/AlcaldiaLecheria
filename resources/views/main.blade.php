18ab8cd491c95f52a2bde8bb8ade51a1c29efef2
leddinlozada223 <leddinlozada@outlook.com>
2024-04-26 11:27:52 -0400

    cambios menores

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,9 +1,34 @@
+@push('styles')
+<style>
+section#contact {
+    background-color: gray;
+    background-image: url("../assets/img/map-image.png") !important;
+    background-repeat: no-repeat;
+    background-position: center;
+}
 
+#header1 {
+    background-image: url("../assets/img/lecheria-bg.jpg");
+    height: 100%;
+    overflow: hidden;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+/**#header2 {
+    background-color: white;
+    background-image: url("../assets/img/alcalde.png");
+    background-repeat: no-repeat;
+    background-position: left;
+    background-size: contain;
+
+}**/
+</style>
+@endpush
 <x-main-layout>
-    <header id="header1">
+    <header id="header1" class="pt-40 pb-56 xl:bg-cover">
         @livewire('main-nav')
         <div
-            class="relative h-[350px] overflow-hidden bg-[url('../assets/img/lecheria-bg.jpg')] bg-cover bg-[50%] bg-no-repeat">
+            class="relative h-[350px] overflow-hidden bg-cover bg-[50%] bg-no-repeat">
             <div
             class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed">
             <div class="flex h-full items-center justify-center">
@@ -15,18 +40,16 @@
             </div>
         </div>
     </header>
-    <header class="bg-white">
-        @livewire('main-nav')
-        <div
-            class="relative h-[350px] overflow-hidden bg-[url('')] bg-cover bg-[50%] bg-no-repeat">
-            <div
-            class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-black/60 bg-fixed">
-            <div class="flex h-full items-center justify-center">
-                <div class="px-6  text-center text-black md:px-12 sm:absolute sm:top-12 sm:left-64 sm:right-1 sm:bottom-25 ">
-                    <h1 class="text-2xl font-bold "> Nacer en Lechería es sinónimo de fe<br>mariana, de amar sus amaneceres y<br>atardeceres. Esta ciudad, me vio nacer<br>y me ha visto crecer trabajando por ella</h1>
-                    <h1 class="text-2xl font-bold ">¡Gracias vecino por la confianza<br>brindada, seguimos juntos!</h1>
-                </div>
+    <header class=" dark:bg-slate-400 max-h-150" id="header2" >
+        <div class="sm:grid sm:grid-rows-2 sm:grid-flow-col grid-cols-1"> 
+            <div class="sm:col-span-2 alig-middle text-center text-black pt-20">
+                  <h1 class="text-2xl font-bold "> Nacer en Lechería es sinónimo de fe<br>mariana, de amar sus amaneceres y<br>atardeceres. Esta ciudad, me vio nacer<br>y me ha visto crecer trabajando por ella<br>¡Gracias vecino por la confianza<br>brindada, seguimos juntos!</h1>
             </div>
+            <div class="sm:row-span-1 sm:object-center dark:text-black">
+                <img src="{{asset('assets/img/firma-alcalde.png')}}" class="h-[15rem] w-max relative">
+            </div>
+            <div class="sm:row-span-3 sm:col-span-2">
+                <img src="{{asset('assets/img/alcalde.png')}}" class="h-[400px] sm:min-h-[600px] px-12">
             </div>
         </div>
     </header>
