ccb5e743e9f30d6e458a067994fc6b63fe90d6d3
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2024-04-29 14:28:22 -0400

    component changes

---
+++ resources/views/components/main-header.blade.php
@@ -0,0 +1,13 @@
+@props(
+    [
+        'bg_img',
+    ]
+)
+
+<header id="header1" class="pt-40 pb-56 xl:bg-cover relative overflow-hidden h-full bg-no-repeat bg-center" style="background-image: url('{{ $bg_img }}')">
+        <div class="flex">
+            <div class=" text-white text-center sm:absolute  sm:bottom-0 sm:right-0 sm:m-6 px-6 md:px-12 font-sans font-black">
+                {{ $slot }}
+            </div>
+        </div>
+</header>
