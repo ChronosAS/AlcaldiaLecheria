077ed1db403009d57aec6684d3e3750b1c1a7756
leddinlozada223 <leddinlozada@outlook.com>
2024-05-02 14:04:36 -0400

    cambio de color del submenu

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -2,9 +2,9 @@
     <x-main-header bg_img="../assets/img/lecheria-bg.jpg">
         <h1 class="text-7xl">Historia</h1>
     </x-main-header>
-    <div class="mt-4 px-4 mx-auto max-w-7xl border-b border-gray-200 dark:border-gray-700 dark:bg-gray-700 sm:rounded-xl">
+    <div class="mt-4 px-4 mx-auto max-w-7xl border-b border-gray-200 dark:border-gray-700 dark:bg-blue-900 sm:rounded-xl">
         <ul 
-            class="flex flex-wrap -mb-px text-lg font-medium text-center items-center justify-evenly"
+            class="flex flex-wrap -mb-px text-lg font-medium text-center items-center justify-evenly text-white"
             id="default-tab" 
             data-tabs-toggle="#default-tab-content" 
             role="tablist"
@@ -12,16 +12,16 @@
             data-tabs-inactive-classes="dark:border-transparent text-white hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300"
         >
             <li class="me-2" role="presentation">
-                <button class="inline-block p-4 border-b-2 rounded-t-lg text-white" id="history-tab" data-tabs-target="#history" type="button" role="tab" aria-controls="history" aria-selected="false">Historia</button>
+                <button class="inline-block p-4 border-b-2 rounded-t-lg dark:text-white" id="history-tab" data-tabs-target="#history" type="button" role="tab" aria-controls="history" aria-selected="false">Historia</button>
             </li>
             <li class="me-2" role="presentation">
-                <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-white" id="hymn-tab" data-tabs-target="#hymn" type="button" role="tab" aria-controls="hymn" aria-selected="false">Himno</button>
+                <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-300 hover:border-gray-300 dark:hover:text-gray-300 dark:text-white" id="hymn-tab" data-tabs-target="#hymn" type="button" role="tab" aria-controls="hymn" aria-selected="false">Himno</button>
             </li>
             <li class="me-2" role="presentation">
-                <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-white" id="shield-tab" data-tabs-target="#shield" type="button" role="tab" aria-controls="shield" aria-selected="false">Escudo</button>
+                <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-300 hover:border-gray-300 dark:hover:text-gray-300 dark:text-white" id="shield-tab" data-tabs-target="#shield" type="button" role="tab" aria-controls="shield" aria-selected="false">Escudo</button>
             </li>
             <li role="presentation">
-                <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-white" id="flag-tab" data-tabs-target="#flag" type="button" role="tab" aria-controls="flag" aria-selected="false">Bandera</button>
+                <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-300 hover:border-gray-300 dark:hover:text-gray-300 dark:text-white" id="flag-tab" data-tabs-target="#flag" type="button" role="tab" aria-controls="flag" aria-selected="false">Bandera</button>
             </li>
         </ul>
     </div>
