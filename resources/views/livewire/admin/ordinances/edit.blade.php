46dc3b1f76c24e386528d5595075feedbcbcdbfa
leddinlozada223 <leddinlozada@outlook.com>
2024-05-20 14:28:12 -0400

    editar

--- resources/views/livewire/admin/ordinances/edit.blade.php
+++ resources/views/livewire/admin/ordinances/edit.blade.php
@@ -1,16 +1,38 @@
 <div>
     {{-- Do your work, then step back. --}}
-<form class="max-w-lg mx-auto">
-    <div class="mb-5">
-        <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Large input</label>
-        <input type="text" id="large-input" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+  <div class="max-w-7xl mx-auto py-10 sm:px-6 mt-5 lg:px-8 dark:bg-blue-900 rounded-lg">
+        <x-form-section submit="create">
+            <x-slot name="title">
+                Detalles
+            </x-slot>
+
+            <x-slot name="description">
+                Edita el Número y fecha de la ordenanza
+            </x-slot>
+
+            <x-slot name="form">
+                <div class="col-span-6 sm:col-span-4">
+                    <x-label for="number" value="Numero" required="true"/>
+                    <x-input id="number" min="0" type="number" style="color: black" class="mt-1 block w-full text-black bg-white dark:bg-white dark:text-black focus:border-blue-500 dark:focus:border-blue-600 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600" wire:model="state.name" autofocus />
+                    <x-input-error for="number" class="mt-2" />
+                </div>
+                <div class="col-span-6 sm:col-span-4">
+                    <x-label for="date" value="Fecha" required="true"/>
+                    <x-input id="date" type="date" style="color: black" class="mt-1 block w-full text-black bg-white dark:bg-white dark:text-black focus:border-blue-500 dark:focus:border-blue-600 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600" wire:model="state.name" autofocus />
+                    <x-input-error for="date" class="mt-2" />
+                </div>
+                <div class="col-span-6 sm:col-span-4">
+                    <x-label for="file" value="Archivo PDF" required="true"/>
+                    <x-input id="file" type="file" style="color: black" class="mt-1 block w-full text-black bg-white dark:bg-white dark:text-black focus:border-blue-500 dark:focus:border-blue-600 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600" wire:model="state.name" autofocus />
+                    <x-input-error for="file" class="mt-2" />
+                </div>
+            </x-slot>
+
+            <x-slot name="actions">
+                <x-button>
+                    Editar
+                </x-button>
+            </x-slot>
+        </x-form-section>
     </div>
-  <div class="mb-5">
-      <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">date</label>
-      <input type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-  </div>
-    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Upload file</label>
-    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file">
-  <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help">A profile picture is useful to confirm your are logged into your account</div>
-</form>
 </div>
