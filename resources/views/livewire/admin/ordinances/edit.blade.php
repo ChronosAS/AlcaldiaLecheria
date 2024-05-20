07a1636a326d9e1b5b4cfcab20b219099fa8fde2
leddinlozada223 <leddinlozada@outlook.com>
2024-05-20 14:22:47 -0400

    Edit

--- resources/views/livewire/admin/ordinances/edit.blade.php
+++ resources/views/livewire/admin/ordinances/edit.blade.php
@@ -1,3 +1,16 @@
 <div>
     {{-- Do your work, then step back. --}}
+<form class="max-w-lg mx-auto">
+    <div class="mb-5">
+        <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Large input</label>
+        <input type="text" id="large-input" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    </div>
+  <div class="mb-5">
+      <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">date</label>
+      <input type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+  </div>
+    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Upload file</label>
+    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file">
+  <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help">A profile picture is useful to confirm your are logged into your account</div>
+</form>
 </div>
