3f7f9732442b9263e1da17939b7b220891732d4f
leddinlozada223 <leddinlozada@outlook.com>
2024-05-24 12:41:36 -0400

    welcome

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,10 +1,12 @@
-<div class=" bg-white dark:bg-blue-950 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
-    <a class="mb-4 me-5 ms-2 mt-3 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 sm:mb-0 sm:mt-0" wire:navigate href="{{ route('app') }}">
-    <img src="{{ asset('assets/img/logo-lecheria.png') }}" class="h-50 w-70" alt="Logo Lecheria" loading="lazy"/>
-    </a>
-    <p class="mt-6 text-white dark:text-white leading-relaxed  text-justify">
-       
-    </p>
+<div class=" flex grid-cols-5 grid-rows-2 gap-4 dark:bg-blue-950 dark:bg-gradient-to-bl dark:from-gray-700/50 border-gray-200 dark:border-gray-700 ">
+    <div class="col-span-3 row-span-2 justify-self-center">
+        <a class="text-neutral-900  hover:text-neutral-900  focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 " wire:navigate href="{{ route('app') }}">
+            <img src="{{ asset('assets/img/logo-lecheria.png') }}" class="mt-2 mb-2"  alt="Logo Lecheria" loading="lazy"/>
+        </a> 
+    </div>
+    <div class="text-left col-span-2 row-span-2 col-start-4 ">
+        <h1 class="mt-8 sm:text-4xl text-4xl text-white dark:text-white font-sans font-black">PANEL<br>ADMINISTRATIVO</h1>
+    </div>
 </div>
 
 <div class="bg-gray-200 dark:bg-blue-900 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
@@ -73,5 +75,5 @@
         <p class="mt-4 text-white dark:text-gray-400 text-sm leading-relaxed text-justify">
             
         </p>
-    </div>
-</div>
+    </div> 
+</div> 
