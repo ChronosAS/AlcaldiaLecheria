5e3ba5a35f5576edaf07bc016bf25b0022081eef
leddinlozada223 <leddinlozada@outlook.com>
2024-05-28 13:47:41 -0400

    news section

--- resources/views/livewire/admin/news/create.blade.php
+++ resources/views/livewire/admin/news/create.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="max-w-7xl mx-auto py-10 sm:px-6 mt-5 lg:px-8 dark:bg-blue-900 rounded-lg">
-        <x-form-section submit="create">
+        <x-one-column-form-section submit="create">
             <x-slot name="title">
                 Detalles
             </x-slot>
@@ -10,22 +10,22 @@
             </x-slot>
 
             <x-slot name="form">
-            <div class="col-span-6 sm:col-span-4">
+                <div class="col-span-6 sm:col-span-3">
                     <x-label for="name" value="Titulo" required="true"/>
                     <x-input id="name"  type="text" style="color: black" class="mt-1 block w-full text-black bg-white dark:bg-white dark:text-black focus:border-blue-500 dark:focus:border-blue-600 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600" wire:model="state.name" autofocus />
                     <x-input-error for="name" class="mt-2" />
                 </div>
-                <div class="col-span-6 sm:col-span-4">
+                <div class="col-span-6 sm:col-span-3">
                     <x-label for="sub_title" value="Sub Titulo" required="true"/>
                     <x-input id="sub_title" min="0" type="text" style="color: black" class="mt-1 block w-full text-black bg-white dark:bg-white dark:text-black focus:border-blue-500 dark:focus:border-blue-600 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600" wire:model="state.name" autofocus />
                     <x-input-error for="sub_title" class="mt-2" />
                 </div>
-                <div class="col-span-6 sm:col-span-4">
+                <div class="col-span-6 sm:col-span-3">
                     <x-label for="date" value="Fecha" required="true"/>
                     <x-input id="date" type="date" style="color: black" class="mt-1 block w-full text-black bg-white dark:bg-white dark:text-black focus:border-blue-500 dark:focus:border-blue-600 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600" wire:model="state.name" autofocus />
                     <x-input-error for="date" class="mt-2" />
                 </div>
-                <div class="col-span-6 sm:col-span-4">
+                <div class="col-span-6 sm:col-span-3">
                     <x-label for="user" value="Creado Por" required="true"/>
                     <select class="mt-1 block w-full text-black bg-white dark:bg-white dark:text-black focus:border-blue-500 dark:focus:border-blue-600 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600 rounded-md shadow-sm" wire:model='user' name="user" id="user">
                         <option value="" selected disabled>Elija uno...</option>
@@ -55,7 +55,7 @@
                     <a class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" wire:navigate href="{{ route('news.index') }}">Regresar</a>
                 </div>
             </x-slot>
-        </x-form-section>
+        </x-one-column-form-section>
     </div>
     @push('styles')
         <style>
