71d411f8f0666bccf721e090bbd9bd02e7590279
leddinlozada223 <leddinlozada@outlook.com>
2024-05-29 14:23:32 -0400

    news

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,11 +1,11 @@
 <x-main-layout class="dark:bg-slate-300">
 <div>
     <x-main-header bg_img="../assets/img/lecheria-bg.jpg">
-        <h1 class="text-7xl" x-text='title' ></h1>
+        <h1 class="text-7xl" x-text='title' >NOTICIAS</h1>
     </x-main-header>
     <x-full-card>
-        <div>
-        </div>
+         <x-slot name="content">
+        </x-slot>
     </x-full-card>
 </div>
 </x-main-layout>
