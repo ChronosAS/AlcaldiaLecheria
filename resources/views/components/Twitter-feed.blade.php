38f5e048d3ae5075b72da11fc19f4738f047454b
leddinlozada223 <leddinlozada@outlook.com>
2024-05-29 09:03:38 -0400

    twitter feed

---
+++ resources/views/components/Twitter-feed.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <a class="twitter-timeline" href="https://twitter.com/Urbanejalcaldia?ref_src=twsrc%5Etfw">Tweets Urbanejalcaldia</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
