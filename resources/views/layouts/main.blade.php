814da37037e2994b3dd1e7be880481f10c4e87af
leddinlozada223 <leddinlozada@outlook.com>
2024-05-30 12:32:25 -0400

    news section and article

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -27,12 +27,12 @@
         <main>
             {{ $slot }}
         </main>
-        <footer class="bg-white shadow m-auto dark:bg-white">
+        <footer class="bg-blue-900 shadow m-auto" >
             <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between sm:items-center sm:justify-center">
-                <span class="flex flex-wrap items-center sm:text text-gray-500 sm:text-center dark:text-gray-400 mx-12">
+                <span class="flex flex-wrap items-center sm:text text-white sm:text-center  mx-12">
                     Copyright © Alcaldía de Lechería 2024
                 </span>
-                <ul class="flex flex-wrap items-center mt-3  font-medium text-gray-500 dark:text-gray-400 sm:text-center">
+                <ul class="flex flex-wrap items-center mt-3  font-medium text-white sm:text-center">
                     <li><a href="{{ route('login') }}" class="hover:underline me-4 md:me-6">Admin</a></li>
                 </ul>
             </div>
--- resources/views/livewire/news/main.blade.php
+++ resources/views/livewire/news/main.blade.php
@@ -1,3 +1,99 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    <x-main-header bg_img="../assets/img/lecheria-bg.jpg">
+        <h1 class="text-7xl" x-text='title' ></h1>
+    </x-main-header>
+      <div class="my-8 space-y-4 px-4 mx-auto max-w-7xl">
+        <div class="bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6 ">
+          <div class="mt-5 space-y-4 md:space-y-6">
+            <section id="Projects" class="w-fit mx-auto  justify-items-center justify-center">
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-10 md:px-20">
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 ">Title</div>
+                                <p class="text-gray-500 ">subtitle</p>
+                                <hr class="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700">
+                                <p class="text-gray-500 ">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+                                <a href="#" class="hover:text-orange-500 text-blue-800">Learn more</a>
+                        </div>
+                    </div>
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 ">Title</div>
+                                <p class="text-gray-500 ">subtitle</p>
+                                <hr class="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700">
+                                <p class="text-gray-500 ">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+                                <a href="#" class="hover:text-orange-500 text-blue-800">Learn more</a>
+                        </div>
+                    </div>
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 ">Title</div>
+                                <p class="text-gray-500 ">subtitle</p>
+                                <hr class="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700">
+                                <p class="text-gray-500 ">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+                                <a href="#" class="hover:text-orange-500 text-blue-800">Learn more</a>
+                        </div>
+                    </div>
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 ">Title</div>
+                                <p class="text-gray-500 ">subtitle</p>
+                                <hr class="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700">
+                                <p class="text-gray-500 ">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+                                <a href="#" class="hover:text-orange-500 text-blue-800">Learn more</a>
+                        </div>
+                    </div>
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 ">Title</div>
+                                <p class="text-gray-500 ">subtitle</p>
+                                <hr class="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700">
+                                <p class="text-gray-500 ">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+                                <a href="#" class="hover:text-orange-500 text-blue-800">Learn more</a>
+                        </div>
+                    </div>
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 ">Title</div>
+                                <p class="text-gray-500 ">subtitle</p>
+                                <hr class="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700">
+                                <p class="text-gray-500 ">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+                                <a href="#" class="hover:text-orange-500 text-blue-800">Learn more</a>
+                        </div>
+                    </div>
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 ">Title</div>
+                                <p class="text-gray-500 ">subtitle</p>
+                                <hr class="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700">
+                                <p class="text-gray-500 ">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+                                <a href="#" class="hover:text-orange-500 text-blue-800">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            </div>
+        </div>
+    </div>
 </div>
