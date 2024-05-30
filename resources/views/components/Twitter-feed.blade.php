95852d1df604ce3a089a06507c816ffe3b2c492c
leddinlozada223 <leddinlozada@outlook.com>
2024-05-30 13:56:03 -0400

    twitter feed

--- resources/views/components/Twitter-feed.blade.php
+++ resources/views/components/Twitter-feed.blade.php
@@ -1,4 +1,4 @@
-<div class=" w-full">
+<div {{ $attributes->merge(['class' => 'w-full']) }}>
     <a class="twitter-timeline" href="https://twitter.com/Urbanejalcaldia?ref_src=twsrc%5Etfw">Tweets Urbanejalcaldia</a>
     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
--- resources/views/livewire/news/main.blade.php
+++ resources/views/livewire/news/main.blade.php
@@ -3,26 +3,100 @@
         <h1 class="text-7xl" x-text='title' ></h1>
     </x-main-header>
       <div class="my-8 space-y-4 px-4 mx-auto max-w-7xl flex gap-9">
-        <div class="bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6 ">
+        <div class="bg-white shadow  sm:rounded-lg sm:p-6 ">
           <div class="mt-5 space-y-4 md:space-y-6">
             <section id="Projects" class="w-fit mx-auto  justify-items-center justify-center">
-                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-10  md:px-20">
+               <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-10 md:px-20">
+
                     <div class="bg-white rounded-xl shadow-md overflow-hidden">
                         <div class="relative">
                             <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                            <div class="absolute top-0 right-0 bg-indigo-500 text-white font-bold px-2 py-1 m-2 rounded-md">New
+                            </div>
+                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
+                            </div>
                         </div>
                         <div class="p-4">
-                            <div class="text-lg font-medium text-gray-800 ">Title</div>
-                                <p class="text-gray-500 ">subtitle</p>
-                                <hr class="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700">
-                                <p class="text-gray-500 ">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
-                                <a href="#" class="hover:text-orange-500 text-blue-800">Learn more</a>
+                            <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
+                                egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
                         </div>
                     </div>
+
+
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
+                            </div>
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
+                                egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
+                        </div>
+                    </div>
+
+
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                            <div class="absolute top-0 right-0 bg-indigo-500 text-white font-bold px-2 py-1 m-2 rounded-md">New</div>
+                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
+                            </div>
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
+                                egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
+                        </div>
+                    </div>
+
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
+                            </div>
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
+                                egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
+                        </div>
+                    </div>
+
+
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
+                            </div>
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
+                                egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
+                        </div>
+                    </div>
+
+
+                    <div class="bg-white rounded-xl shadow-md overflow-hidden">
+                        <div class="relative">
+                            <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
+                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
+                            </div>
+                        </div>
+                        <div class="p-4">
+                            <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
+                                egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
+                        </div>
+                    </div>
+
                 </div>
             </section>
             </div>
         </div>
-        <x-twitter-feed/>
+        <x-twitter-feed class="hidden sm:inline-block"/>
     </div>
 </div>
