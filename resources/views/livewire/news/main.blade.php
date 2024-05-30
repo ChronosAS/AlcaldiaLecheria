1f92a67d9aab2d7d82e0498ab74219888900b006
leddinlozada223 <leddinlozada@outlook.com>
2024-05-30 13:59:53 -0400

    organize

--- resources/views/livewire/news/main.blade.php
+++ resources/views/livewire/news/main.blade.php
@@ -7,46 +7,38 @@
           <div class="mt-5 space-y-4 md:space-y-6">
             <section id="Projects" class="w-fit mx-auto  justify-items-center justify-center">
                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-10 md:px-20">
-
                     <div class="bg-white rounded-xl shadow-md overflow-hidden">
                         <div class="relative">
                             <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
-                            <div class="absolute top-0 right-0 bg-indigo-500 text-white font-bold px-2 py-1 m-2 rounded-md">New
-                            </div>
-                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
-                            </div>
                         </div>
                         <div class="p-4">
                             <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+                            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
                             <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
                                 egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
                         </div>
                     </div>
-
-
                     <div class="bg-white rounded-xl shadow-md overflow-hidden">
                         <div class="relative">
                             <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
-                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
-                            </div>
                         </div>
                         <div class="p-4">
                             <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+                            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
                             <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
                                 egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
                         </div>
                     </div>
-
-
                     <div class="bg-white rounded-xl shadow-md overflow-hidden">
                         <div class="relative">
                             <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
-                            <div class="absolute top-0 right-0 bg-indigo-500 text-white font-bold px-2 py-1 m-2 rounded-md">New</div>
-                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
-                            </div>
                         </div>
                         <div class="p-4">
                             <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+                            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
                             <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
                                 egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
                         </div>
@@ -55,44 +47,39 @@
                     <div class="bg-white rounded-xl shadow-md overflow-hidden">
                         <div class="relative">
                             <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
-                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
-                            </div>
                         </div>
                         <div class="p-4">
                             <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+                            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
                             <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
                                 egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
                         </div>
                     </div>
-
-
                     <div class="bg-white rounded-xl shadow-md overflow-hidden">
                         <div class="relative">
                             <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
-                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
-                            </div>
                         </div>
                         <div class="p-4">
                             <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+                            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
                             <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
                                 egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
                         </div>
                     </div>
-
-
                     <div class="bg-white rounded-xl shadow-md overflow-hidden">
                         <div class="relative">
                             <img class="w-full h-48 object-cover" src="https://via.placeholder.com/600x360">
-                            <div class="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">3 min read
-                            </div>
                         </div>
                         <div class="p-4">
                             <div class="text-lg font-medium text-gray-800 mb-2">Title</div>
+                            <p class="text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+                            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
                             <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, mi sed
                                 egestas tincidunt, libero dolor bibendum nisl, non aliquam quam massa id lacus.</p>
                         </div>
                     </div>
-
                 </div>
             </section>
             </div>
