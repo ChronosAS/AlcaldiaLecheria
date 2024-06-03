ea73ca742a5b66d2d79dcfa04c7619956c670664
leddinlozada223 <leddinlozada@outlook.com>
2024-06-03 10:39:57 -0400

    checkbox

--- resources/views/livewire/admin/news/create.blade.php
+++ resources/views/livewire/admin/news/create.blade.php
@@ -107,8 +107,14 @@
                 </div>
             </x-slot>
             <x-slot name="actions">
-                <div class="">
-                    <x-button data-twe-ripple-init data-twe-ripple-color="light">
+                <div class="flex mx-3 space-x-3 items-center">
+                   <div  class="inline-flex text-white">
+                        <x-checkbox id="draft" name="draft" class="text-blue-600 border-blue-300 focus:ring-blue-500"/>
+                        <label for="draft" class="cursor-pointer ms-2 text-sm font-medium">
+                            Borrador
+                        </label>
+                   </div>           
+                    <x-button>
                         Agregar
                     </x-button>
                     <a class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" wire:navigate href="{{ route('admin.news.index') }}">Regresar</a>
