cfe89805d8959711491505d0058c59e9f5b6bbc3
leddinlozada223 <leddinlozada@outlook.com>
2024-06-03 09:00:48 -0400

    carousel slider and alt image

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -27,13 +27,13 @@
         <header class=" dark:bg-white max-h-150" id="header2" >
             <div class="place-items-center sm:grid sm:grid-cols-5 sm:grid-rows-5 gap-4">
                 <div class="sm:col-span-2 sm:row-span-5 sm:h-auto sm:w-auto h-0 w-0">
-                    <img src="{{asset('assets/img/alcalde.png')}}" class="sm:h-[40rem]">
+                    <img src="{{asset('assets/img/alcalde.png')}}" class="sm:h-[40rem]" alt="Alcalde Manuel Ferreira">
                 </div>
                 <div class="sm:col-span-3 sm:row-span-3 col-start-3 sm:object-center text-center text-black pt-20 font-sans">
                       <h1 class="sm:text-4xl text-xl font-bold "> Nacer en Lechería es sinónimo de fe<br>mariana, de amar sus amaneceres y<br>atardeceres. Esta ciudad, me vio nacer<br>y me ha visto crecer trabajando por ella<br>¡Gracias vecino por la confianza<br>brindada, seguimos juntos!</h1>
                 </div>
                 <div class="sm:col-span-3 sm:row-span-2 col-start-3 row-start-4 flex items-center justify-center">
-                    <img src="{{asset('assets/img/firma-alcalde-b.png')}}" class="h-[15rem] w-max ">
+                    <img src="{{asset('assets/img/firma-alcalde-b.png')}}" class="h-[15rem] w-max " alt="Firma del Alcalde">
                 </div>
             </div>
         </header>
