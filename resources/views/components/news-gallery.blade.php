2a4f49b2b191e8dfb734681321d50c642c0d6754
leddinlozada223 <leddinlozada@outlook.com>
2024-06-05 09:06:53 -0400

    mobile collapse

--- resources/views/components/news-gallery.blade.php
+++ resources/views/components/news-gallery.blade.php
@@ -4,10 +4,10 @@
         <h1> Noticias Recientes </h1>
         </div>
         <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-            <div class="grid grid-cols-2 r gap-4 my-8">
+            <div class="grid grid-cols-1 r gap-4 my-8 sm:grid-cols-2 flex-wrap flex-col">
                     <!-- image - start -->
                     <a href=""
-                        class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                        class=" relative flex  h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
                         <img src="https://images.unsplash.com/2/03.jpg?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" loading="lazy"  class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
                         <div
                             class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
@@ -17,7 +17,7 @@
                         </span>
                     </a>
                      <a href=""
-                        class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                        class="relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
                         <img src="https://images.unsplash.com/2/03.jpg?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" loading="lazy"  class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
                         <div
                             class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
@@ -28,10 +28,10 @@
                     </a>
                     <!-- image - end -->
             </div>
-            <div class="grid grid-cols-3 gap-4">
+            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 flex-wrap flex-col">
                         <!-- image - start -->
                     <a href="#"
-                        class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg  md:h-80">
+                        class=" relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg  md:h-80">
                         <img src="https://images.unsplash.com/2/01.jpg?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" loading="lazy"  class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
                         <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
                           <span class="relative ml-4 mb-3 inline-block font-bold text-sm text-white  sm:text-lg md:ml-5 ">titulo<br> 
@@ -42,7 +42,7 @@
 
                     <!-- image - start -->
                     <a href="#"
-                        class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg  md:h-80">
+                        class=" relative flex  h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg  md:h-80">
                         <img src="https://images.unsplash.com/2/09.jpg?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" loading="lazy"  class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
                         <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
                           <span class="relative ml-4 mb-3 inline-block font-bold text-sm text-white  sm:text-lg md:ml-5 ">titulo<br>   
@@ -53,7 +53,7 @@
 
                     <!-- image - start -->
                     <a href="#"
-                        class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                        class="relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
                         <img src="https://images.unsplash.com/2/05.jpg?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" loading="lazy" r class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
                         <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
                         <span class="relative ml-4 mb-3 inline-block font-bold text-sm text-white  sm:text-lg md:ml-5 ">titulo<br> 
