35b51a58fd4dc0b9e3e0f2fb00954b3de521cf23
leddinlozada223 <leddinlozada@outlook.com>
2024-06-11 12:42:25 -0400

    main news large monitors

--- resources/views/components/news/main.blade.php
+++ resources/views/components/news/main.blade.php
@@ -4,14 +4,14 @@
             <div class="text-center py-8 space-y-3">
                 <h1 class="section-heading text-uppercase text-[3rem] text-white ">Noticias</h1>
             </div>
-            <div class="flex-c sm:grid sm:grid-cols-10 sm:grid-rows-6 sm:gap-4 sm:mx-10">
+            <div class="flex-c sm:grid sm:grid-cols-10 sm:grid-rows-6 sm:gap-4 lg:gap-2 sm:mx-10 mb-10">
                 <div class="col-span-8 row-span-4">
                     <div class="w-full h-full text-md sm:text-md ">
                         @livewire('news.news-carousel',['posts'=>$posts])
                     </div>
                 </div>
                 <div class="col-span-2 row-span-2 col-start-9">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
@@ -19,8 +19,8 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-span-2 row-span-2 col-start-1 row-start-5">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                <div class="col-span-2 row-span-2 col-start-1 row-start-5 ">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
@@ -28,8 +28,8 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-span-2 row-span-2 col-start-3 row-start-5">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                <div class="col-span-2 row-span-2 col-start-3 row-start-5 lg:gap-2">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
@@ -37,8 +37,8 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-span-2 row-span-2 col-start-5 row-start-5">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                <div class="col-span-2 row-span-2 col-start-5 row-start-5 lg:gap-2">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
@@ -46,8 +46,8 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-span-2 row-span-2 col-start-7 row-start-5">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                <div class="col-span-2 row-span-2 col-start-7 row-start-5 lg:gap-2">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
@@ -55,8 +55,8 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-span-2 row-span-2 col-start-9 row-start-5">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                <div class="col-span-2 row-span-2 col-start-9 row-start-5 lg:gap-2">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
@@ -64,8 +64,8 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-span-2 row-span-2 col-start-9 row-start-3">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                <div class="col-span-2 row-span-2 col-start-9 row-start-3 lg:gap-2">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
