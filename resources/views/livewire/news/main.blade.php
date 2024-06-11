f24cc30e069a09f2470c29b83e9762be6da030e8
leddinlozada223 <leddinlozada@outlook.com>
2024-06-11 08:59:09 -0400

    noticias main mobile  view

--- resources/views/livewire/news/main.blade.php
+++ resources/views/livewire/news/main.blade.php
@@ -3,60 +3,65 @@
         <h1 class="text-7xl" >Noticias</h1>
     </x-main-header>
     <x-full-card>
+        <x-slot name="title">
+            <div class=" text-3xl">
+                   
+            </div>
+        </x-slot>
         <x-slot name="content" class="">
-            <div class="grid grid-cols-5 grid-rows-2 gap-4">
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
+            <div class=" grid grid-cols-1 sm:grid-cols-5 sm:grid-rows-2 sm:gap-4">
+                <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="" alt="">
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <div class="grid justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "></h5>
                         <p class="mb-3 font-normal text-gray-900"></p>
                     </div>
                 </a>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
+                <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="" alt="">
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <div class="grid justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "></h5>
                         <p class="mb-3 font-normal text-gray-900"></p>
                     </div>
                 </a>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
+                <a href="#" class="grid items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="" alt="">
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <div class="grid justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "></h5>
                         <p class="mb-3 font-normal text-gray-900"></p>
                     </div>
                 </a>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
+                <a href="#" class="grid items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="" alt="">
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <div class="grid justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "></h5>
                         <p class="mb-3 font-normal text-gray-900"></p>
                     </div>
                 </a>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
+                <a href="#" class="grid items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="" alt="">
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <div class="grid justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "></h5>
                         <p class="mb-3 font-normal text-gray-900"></p>
                     </div>
                 </a>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
+                <a href="#" class="grid items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="" alt="">
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <div class="grid justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "></h5>
                         <p class="mb-3 font-normal text-gray-900"></p>
                     </div>
                 </a>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
+                <a href="#" class="grid items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="" alt="">
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <div class="grid justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "></h5>
                         <p class="mb-3 font-normal text-gray-900"></p>
                     </div>
                 </a>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
+                <a href="#" class="grid items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="" alt="">
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <div class="grid justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "></h5>
                         <p class="mb-3 font-normal text-gray-900"></p>
                     </div>
