b93ab0ea01371dd6bcda5192f6783bfab6d31ed6
leddinlozada223 <leddinlozada@outlook.com>
2024-06-11 14:32:47 -0400

    news centered

--- resources/views/components/news/main.blade.php
+++ resources/views/components/news/main.blade.php
@@ -4,13 +4,14 @@
             <div class="text-center py-8 space-y-3">
                 <h1 class="section-heading text-uppercase text-[3rem] text-white ">Noticias</h1>
             </div>
+   
             <div class="flex-c sm:grid sm:grid-cols-10 sm:grid-rows-6 sm:gap-4 lg:gap-2 sm:mx-10 mb-10">
-                <div class="col-span-8 row-span-4">
-                    <div class="w-full h-full text-md sm:text-md ">
+                <div class="col-span-4 row-span-4 col-start-3">
+                    <div class="w-full h-full text-md sm:text-md justify-self-center">
                         @livewire('news.news-carousel',['posts'=>$posts])
                     </div>
                 </div>
-                <div class="col-span-2 row-span-2 col-start-9">
+                <div class="col-span-2 row-span-2 col-start-7">
                     <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
@@ -19,25 +20,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-span-2 row-span-2 col-start-1 row-start-5 ">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
-                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
-                        <div class="grid justify-between p-4 leading-normal">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
-                            <p class="mb-3 font-normal text-gray-900">subtitulo</p>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-span-2 row-span-2 col-start-3 row-start-5 lg:gap-2">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
-                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
-                        <div class="grid justify-between p-4 leading-normal">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
-                            <p class="mb-3 font-normal text-gray-900">subtitulo</p>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-span-2 row-span-2 col-start-5 row-start-5 lg:gap-2">
+                <div class="col-span-2 row-span-2 col-start-7 row-start-3 ">
                     <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
@@ -55,7 +38,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-span-2 row-span-2 col-start-9 row-start-5 lg:gap-2">
+                <div class="col-span-2 row-span-2 col-start-5 row-start-5 lg:gap-2">
                     <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
@@ -64,7 +47,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-span-2 row-span-2 col-start-9 row-start-3 lg:gap-2">
+                <div class="col-span-2 row-span-2 col-start-3 row-start-5 lg:gap-2">
                     <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
                         <div class="grid justify-between p-4 leading-normal">
@@ -74,7 +57,6 @@
                     </a>
                 </div>
             </div>
-
         </div>
     </div>
 </section>
