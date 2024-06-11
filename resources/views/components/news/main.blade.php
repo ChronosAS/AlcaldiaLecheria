d797e572fbacc561d46c3b1401617f95dd17482b
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2024-06-11 12:01:04 -0400

    main news section

--- resources/views/components/news/main.blade.php
+++ resources/views/components/news/main.blade.php
@@ -1,14 +1,80 @@
-<div>
-    <section id="news">
-        <div class="text-center">
-            <div class=" sm:justify-center">
-                <div class="text-center py-8 space-y-3">
-                    <h1 class="section-heading text-uppercase text-[3rem] text-white ">Noticias</h1>
+<section id="news">
+    <div class="text-center">
+        <div class=" sm:justify-center">
+            <div class="text-center py-8 space-y-3">
+                <h1 class="section-heading text-uppercase text-[3rem] text-white ">Noticias</h1>
+            </div>
+            <div class="flex-c sm:grid sm:grid-cols-10 sm:grid-rows-6 sm:gap-4 sm:mx-10">
+                <div class="col-span-8 row-span-4">
+                    <div class="w-full h-full text-md sm:text-md ">
+                        @livewire('news.news-carousel',['posts'=>$posts])
+                    </div>
                 </div>
-                <div class="sm:mx-[25%] sm:mb-5 sm:w-[50%] w-full text-md sm:text-md ">
-                    @livewire('news.news-carousel',['posts'=>$posts])
+                <div class="col-span-2 row-span-2 col-start-9">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
+                        <div class="grid justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
+                            <p class="mb-3 font-normal text-gray-900">subtitulo</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-span-2 row-span-2 col-start-1 row-start-5">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
+                        <div class="grid justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
+                            <p class="mb-3 font-normal text-gray-900">subtitulo</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-span-2 row-span-2 col-start-3 row-start-5">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
+                        <div class="grid justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
+                            <p class="mb-3 font-normal text-gray-900">subtitulo</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-span-2 row-span-2 col-start-5 row-start-5">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
+                        <div class="grid justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
+                            <p class="mb-3 font-normal text-gray-900">subtitulo</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-span-2 row-span-2 col-start-7 row-start-5">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
+                        <div class="grid justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
+                            <p class="mb-3 font-normal text-gray-900">subtitulo</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-span-2 row-span-2 col-start-9 row-start-5">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
+                        <div class="grid justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
+                            <p class="mb-3 font-normal text-gray-900">subtitulo</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-span-2 row-span-2 col-start-9 row-start-3">
+                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
+                        <div class="grid justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
+                            <p class="mb-3 font-normal text-gray-900">subtitulo</p>
+                        </div>
+                    </a>
                 </div>
             </div>
+
         </div>
-    </section>
-</div>
+    </div>
+</section>
