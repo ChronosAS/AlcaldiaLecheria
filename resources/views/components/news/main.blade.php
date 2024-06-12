5524b8108eee55bad43bfbc1b19c9e0f541fec37
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2024-06-12 13:01:56 -0400

    merge fix

--- resources/views/components/news/main.blade.php
+++ resources/views/components/news/main.blade.php
@@ -4,9 +4,15 @@
             <div class="text-center py-8 space-y-3">
                 <h1 class="section-heading text-uppercase text-[3rem] text-white ">Noticias</h1>
             </div>
-            <div class="flex-c sm:grid sm:grid-cols-10 sm:grid-rows-6 sm:gap-4 sm:mx-10">
-                <div class="col-span-8 row-span-4">
-                    <div id="newsCarousel" posts="{{ json_encode($posts) }}"></div>
+            <div class="sm:flex md:hidden lg:hidden xl:hidden">
+
+            </div>
+            <div class="hidden sm:flex-wrap sm:grid sm:grid-cols-10 sm:grid-rows-6 sm:gap-4 lg:gap-2 sm:mx-10 mb-10">
+
+                <div class="sm:col-span-4 sm:row-span-4 sm:col-start-3">
+                    <div class="sm:w-full sm:h-full text-md sm:text-md justify-self-center">
+                        <div class="" id="newsCarousel" posts="{{ json_encode($posts) }}"></div>
+                    </div>
                 </div>
                 <div class="flex-wrap sm:flex-wrap sm:col-span-2 sm:row-span-4 sm:col-start-7 hidden sm:inline-block">
                     <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl min-h-full lg:size-[340px] hover:bg-gray-100 ">
