ccdd8522f240efb723fb8830ce9cc3eca1e712bd
leddinlozada223 <leddinlozada@outlook.com>
2024-06-17 14:07:26 -0400

    index change

--- resources/views/livewire/news/index.blade.php
+++ resources/views/livewire/news/index.blade.php
@@ -2,33 +2,85 @@
     <x-main-header bg_img="../assets/img/lecheria-bg.jpg">
         <h1 class="text-7xl" >Noticias</h1>
     </x-main-header>
-    <div class="my-8 mt-0 space-y-4 p-4 mx-auto max-w-7xl">
-        <x-full-card>
-            <x-slot name="title">
-                <div class=" text-3xl">
-                    
-                </div>
-            </x-slot>
-            <x-slot name="content" class="">
-                <div class=" grid grid-cols-1 sm:grid-cols-5 sm:grid-rows-2 sm:gap-4">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
-                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-none md:rounded-s-lg" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
-                        <div class="grid justify-between p-4 leading-normal">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Titulo</h5>
-                            <p class="mb-3 font-normal text-gray-900">subtitulo</p>
-                        </div>
-                    </a>
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl hover:bg-gray-100 ">
-                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="" alt="">
-                        <div class="grid justify-between p-4 leading-normal">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "></h5>
-                            <p class="mb-3 font-normal text-gray-900"></p>
-                        </div>
-                    </a>
-                
-                    <x-twitter-feed class="hidden sm:inline-block row-span-2 col-start-5 row-start-1"/>
-                <div>
-            </x-slot>
-        </x-full-card>
-    </div>
+    
+    <div class=" flex sm:grid sm:grid-cols-10 sm:grid-rows-6 gap-4">
+        <div class="sm:col-span-8 sm:row-span-6 my-8 mt-0 space-y-4 p-4 mx-auto max-w-7xl">
+            <x-full-card class="">
+                <x-slot name="title">
+                    <div class=" text-3xl">
+                        
+                    </div>
+                </x-slot>
+                <x-slot name="content" class="">
+                    <!-- inspired by tailwindcss.com -->
+                    <ul class="grid grid-cols-1 xl:grid-cols-3 gap-y-10 gap-x-6 items-start p-8">
+                        <li class="relative flex flex-col sm:flex-row xl:flex-col items-start">
+                            <div class="order-1 sm:ml-6 xl:ml-0">
+                                <h3 class="mb-1 text-slate-900 font-semibold">
+                                    <span class="mb-1 block text-sm leading-6 text-indigo-500">Headless UI</span>Completely unstyled, fully
+                                    accessible UI components
+                                </h3>
+                                <div class="prose prose-slate prose-sm text-slate-600">
+                                    <p>Completely unstyled, fully accessible UI components, designed to integrate beautifully with Tailwind
+                                        CSS.</p>
+                                </div><a
+                                    class="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 mt-6"
+                                    href="">Learn
+                                    more<span class="sr-only">, Completely unstyled, fully accessible UI components</span>
+                                    <svg class="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400"
+                                        width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M0 0L3 3L0 6"></path>
+                                    </svg></a>
+                            </div>
+                            <img src="" alt="" class="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640">
+                        </li>
+                        <li class="relative flex flex-col sm:flex-row xl:flex-col items-start">
+                            <div class="order-1 sm:ml-6 xl:ml-0">
+                                <h3 class="mb-1 text-slate-900 font-semibold">
+                                    <span class="mb-1 block text-sm leading-6 text-purple-500">Heroicons</span>Beautiful hand-crafted SVG
+                                    icons, by the makers of Tailwind CSS.
+                                </h3>
+                                <div class="prose prose-slate prose-sm text-slate-600">
+                                    <p>A set of 450+ free MIT-licensed SVG icons. Available as basic SVG icons and via first-party React and
+                                        Vue libraries.</p>
+                                </div><a
+                                    class="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 mt-6"
+                                    href="">Learn
+                                    more<span class="sr-only">, Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.</span>
+                                    <svg class="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400"
+                                        width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M0 0L3 3L0 6"></path>
+                                    </svg></a>
+                            </div>
+                            <img src="g" alt="" class="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640">
+                        </li>
+                        <li class="relative flex flex-col sm:flex-row xl:flex-col items-start">
+                            <div class="order-1 sm:ml-6 xl:ml-0">
+                                <h3 class="mb-1 text-slate-900 font-semibold">
+                                    <span class="mb-1 block text-sm leading-6 text-cyan-500">Hero Patterns</span>Seamless SVG background
+                                    patterns by the makers of Tailwind CSS.
+                                </h3>
+                                <div class="prose prose-slate prose-sm text-slate-600">
+                                    <p>A collection of over 100 free MIT-licensed high-quality SVG patterns for you to use in your web
+                                        projects.</p>
+                                </div><a
+                                    class="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 mt-6"
+                                    href="">Learn
+                                    more<span class="sr-only">, Seamless SVG background patterns by the makers of Tailwind CSS.</span>
+                                    <svg class="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400"
+                                        width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M0 0L3 3L0 6"></path>
+                                    </svg></a>
+                            </div>
+                            <img src="" alt="" class="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640">
+                        </li>
+                    </ul>
+                </x-slot>
+            </x-full-card>
+        </div>
+        <x-twitter-feed class="col-span-2 row-span-6 col-start-9  sticky hidden sm:inline-block"/>
+    </div>    
 </div>
