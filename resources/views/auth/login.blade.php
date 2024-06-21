c1f59e950d20e86f5fa3d9b747b0d6aafe04d907
leddinlozada223 <leddinlozada@outlook.com>
2024-06-21 12:50:45 -0400

    No account?

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,8 +31,12 @@
                     <span class="ms-2 text-sm text-white dark:text-white ">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
+            <div class="">
+                <a class="underline text-sm text-white dark:text-white hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                    ¿No tienes una cuenta?
+                </a>
+            </div>
+            <div class="flex items-center justify-between ">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-white dark:text-white hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
