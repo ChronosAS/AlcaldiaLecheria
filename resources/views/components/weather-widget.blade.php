88b14fc722cc7c6475851face671ee854434c7eb
leddinlozada223 <leddinlozada@outlook.com>
2024-06-21 11:12:13 -0400

    WEATHER REPORT

--- resources/views/components/weather-widget.blade.php
+++ resources/views/components/weather-widget.blade.php
@@ -1,7 +1,6 @@
 <div class="">
-    <a class="weatherwidget-io" href="https://forecast7.com/es/10d19n64d69/lecheria/" data-label_1="LECHERIA" data-font="Noto Sans" data-days="3" data-textcolor="white" data-suncolor="#fce300" data-mooncolor="#fcb100" data-cloudcolor="#adc9ed" data-cloudfill="#adc9ed" data-raincolor="#3483eb" data-snowcolor="#d6d2d2" >LECHERIA
-        <script>
-        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-        </script>
-    </a>
+   <a class="weatherwidget-io w-[324px]" href="https://forecast7.com/es/10d19n64d69/lecheria/" data-label_1="LECHERIA" data-font="Noto Sans" data-days="3" data-textcolor="white" data-suncolor="#fce300" data-mooncolor="#fcb100" data-cloudcolor="#adc9ed" data-cloudfill="#adc9ed" data-raincolor="#3483eb" data-snowcolor="#d6d2d2" >LECHERIA</a>
+    <script>
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+    </script>
 </div>
