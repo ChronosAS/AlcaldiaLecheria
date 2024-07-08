9cab9f729c0cff4bfd00b77bc775506a27657434
leddinlozada223 <leddinlozada@outlook.com>
2024-07-08 13:12:03 -0400

    border none

--- resources/views/components/multiple-select.blade.php
+++ resources/views/components/multiple-select.blade.php
@@ -77,16 +77,16 @@
     <option value="7">Option 7</option>
     <option value="8">Option 8</option>
 </select>
-<div x-data="dropdown()" x-init="loadOptions()" class="">
+<div x-data="dropdown" x-init="loadOptions()" class="">
     <input name="values" type="hidden" x-bind:value="selectedValues()">
     <div class="inline-block relative w-96">
         <div class="flex flex-col items-center relative">
             <div x-on:click="open" class="w-full">
-                <div class="my-2 p-1 flex border border-gray-200 bg-white rounded">
+                <div class="my-2 p-1 flex  bg-white rounded">
                     <div class="flex flex-auto flex-wrap">
                         <template x-for="(option,index) in selected" :key="options[option].value">
-                            <div class="flex justify-center items-center m-1 font-medium py-1 px-1 bg-white rounded  border">
-                                <div class="text-xs font-normal leading-none max-w-full flex-initial x-model=" options[option] x-text="options[option].text"></div>
+                            <div class="flex justify-center items-center m-1 font-medium py-1 px-1 bg-white rounded  ">
+                                <div class="text-xs font-normal  max-w-full flex-initial x-model=" options[option] x-text="options[option].text"></div>
                                 <div class="flex flex-auto flex-row-reverse">
                                     <div x-on:click.stop="remove(index,option)">
                                         <svg class="fill-current h-4 w-4 " role="button" viewBox="0 0 20 20">
@@ -100,18 +100,18 @@
                             </div>
                         </template>
                         <div x-show="selected.length == 0" class="flex-1">
-                            <input placeholder="Select a option" class="bg-transparent p-1 px-2 appearance-none outline-none h-full w-full text-gray-800" x-bind:value="selectedValues()">
+                            <input placeholder="Selecciona una etiqueta" class="bg-white p-1 px-2 outline-none text-justify h-full w-full text-black" x-bind:value="selectedValues()">
                         </div>
                     </div>
-                    <div class="text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200 svelte-1l8159u">
-                        <button type="button" x-show="isOpen() === true" x-on:click="open" class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
+                    <div class="text-gray-300 w-8 py-1 pl-2 pr-1  flex items-center  ">
+                        <button type="button" x-show="isOpen() === true" x-on:click="open" class="cursor-pointer w-6 h-6 text-black outline-none focus:outline-none">
                             <svg version="1.1" class="fill-current h-4 w-4" viewBox="0 0 20 20">
                                 <path d="M17.418,6.109c0.272-0.268,0.709-0.268,0.979,0s0.271,0.701,0,0.969l-7.908,7.83
                                 c-0.27,0.268-0.707,0.268-0.979,0l-7.908-7.83c-0.27-0.268-0.27-0.701,0-0.969c0.271-0.268,0.709-0.268,0.979,0L10,13.25
                                 L17.418,6.109z" />
                             </svg>
                         </button>
-                        <button type="button" x-show="isOpen() === false" @click="close" class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
+                        <button type="button" x-show="isOpen() === false" @click="close" class="cursor-pointer w-6 h-6 text-black outline-none focus:outline-none">
                             <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
                                 <path d="M2.582,13.891c-0.272,0.268-0.709,0.268-0.979,0s-0.271-0.701,0-0.969l7.908-7.83
                                 c0.27-0.268,0.707-0.268,0.979,0l7.908,7.83c0.27,0.268,0.27,0.701,0,0.969c-0.271,0.268-0.709,0.268-0.978,0L10,6.75L2.582,13.891z
@@ -126,8 +126,8 @@
             <div x-show.transition.origin.top="isOpen()" class="absolute shadow top-100 bg-white z-40 w-full left-0 rounded max-h-select" x-on:click.away="close">
                 <div class="flex flex-col w-full overflow-y-auto h-64">
                     <template x-for="(option,index) in options" :key="option" class="overflow-auto">
-                        <div class="cursor-pointer w-full border-gray-100 rounded-t border-b hover:bg-gray-100" @click="select(index,$event)">
-                            <div class="flex w-full items-center p-2 pl-2 border-transparent border-l-2 relative">
+                        <div class="cursor-pointer w-full  rounded-t  " @click="select(index,$event)">
+                            <div class="flex w-full items-center p-2 pl-2 relative">
                                 <div class="w-full items-center flex justify-between">
                                     <div class="mx-2 leading-6" x-model="option" x-text="option.text"></div>
                                     <div x-show="option.selected">
