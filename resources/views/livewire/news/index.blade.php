c9827fcc6d2841d11af90115fd8d10395cf39571
leddinlozada223 <leddinlozada@outlook.com>
2024-07-15 10:50:48 -0400

    not so small twitter feed

--- resources/views/livewire/news/index.blade.php
+++ resources/views/livewire/news/index.blade.php
@@ -47,7 +47,7 @@
             </x-full-card>
             {{ $this->posts->links('vendor.livewire.tailwind') }}
         </div>
-        <div wire:ignore class="row-start-1 col-start-9    xl:scale-[.7] md:scale-[.7] 2xl:scale-100   xl:w-[14rem]  2xl:w-[25rem] overscroll-y-contain 2xl:bottom-auto xl:bottom-8 right-10 justify-self-center fixed hidden sm:inline-block">
+        <div wire:ignore class="row-start-1 col-start-9    xl:scale-[.8] md:scale-[.7] 2xl:scale-100   xl:w-[14rem]  2xl:w-[25rem] overscroll-y-contain 2xl:bottom-auto xl:bottom-8 right-10 justify-self-center fixed hidden sm:inline-block">
             <a class="twitter-timeline" href="https://twitter.com/Urbanejalcaldia?ref_src=twsrc%5Etfw">Tweets Urbanejalcaldia</a>
         </div>
     </div>
