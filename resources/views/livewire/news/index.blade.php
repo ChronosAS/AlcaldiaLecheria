f81964324c7e0c3fea07570734626a06d3aafcdd
leddinlozada223 <leddinlozada@outlook.com>
2024-07-16 13:29:33 -0400

    lets go

--- resources/views/livewire/news/index.blade.php
+++ resources/views/livewire/news/index.blade.php
@@ -17,9 +17,9 @@
                        
                         <div wire:ignore class="flex mt-2 sm:mt-1 ">
                             <button id="tags" data-dropdown-toggle="tagsDropdown"
-                              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm mx-3 sm:mx-3 p-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full  text-sm mx-3 sm:mx-3 px-4 p-1 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                               type="button">
-                              Filtrar por Etiqueta
+                               Etiquetas
                               <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
