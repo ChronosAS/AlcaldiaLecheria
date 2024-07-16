b546569e95f0aa9e5876ac4196e43bc3153a9242
leddinlozada223 <leddinlozada@outlook.com>
2024-07-16 12:09:46 -0400

    small changes tag index

--- resources/views/livewire/news/index.blade.php
+++ resources/views/livewire/news/index.blade.php
@@ -7,17 +7,17 @@
         <div class="sm:col-span-8 sm:row-span-6 my-8 mt-0 space-y-4 p-4 mx-auto max-w-7xl">
             <x-full-card class="">
                 <x-slot name="title">
-                    <div class=" text-3xl grid sm:grid-cols-2 sm:grid-rows-1 sm:gap-4">
-                        <div class="relative">
+                    <div class=" text-3xl grid grid-cols-1 sm:grid-cols-3 sm:grid-rows-1 sm:gap-4">
+                        <div class="relative sm:mt-1 mt-2">
                             <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
                                 <svg class="w-5 h-5 text-black" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                             </div>
                             <input wire:model.live='search' type="text" name="search" id="search" class=" flex p-2 ps-10 text-sm text-gray-900 border border-black rounded-lg  w-full sm:w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white-700   " placeholder="Buscar...">
-
                         </div>
-                        <div wire:ignore>
+                       
+                        <div wire:ignore class="flex mt-2 sm:mt-1 ">
                             <button id="tags" data-dropdown-toggle="tagsDropdown"
-                              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm mx-3 sm:mx-3 p-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                               type="button">
                               Filtrar por Etiqueta
                               <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24"
@@ -27,7 +27,7 @@
                             </button>
 
                             <!-- Dropdown menu -->
-                            <div id="tagsDropdown" class="z-10 hidden w-56 p-3 bg-white rounded-lg border border-gray-500 shadow ">
+                            <div id="tagsDropdown" class=" hidden sm:w-[18rem] p-3 bg-white rounded-lg border border-gray-500 shadow z-[5]">
                                 <h6 class="mb-3 text-sm font-medium text-gray-900">
                                     Etiquetas
                                 </h6>
@@ -49,8 +49,8 @@
 
                                 </ul>
                             </div>
+                            <x-input id="date" type="date" style="color: black" class="mt-2 sm:mt-1 flex  w-full sm:w-[10rem]  text-black bg-white dark:bg-white dark:text-black focus:border-orange-500 dark:focus:border-orange-400 focus:ring-2 focus:ring-orange-500 dark:focus:ring-orange-500" wire:model.live="date" />
                         </div>
-                        <x-input id="date" type="date" style="color: black" class="mt-1 flex  w-full sm:w-[10rem] sm:col-start-3 text-black bg-white dark:bg-white dark:text-black focus:border-orange-500 dark:focus:border-orange-400 focus:ring-2 focus:ring-orange-500 dark:focus:ring-orange-500" wire:model.live="date" />
                     </div>
                 </x-slot>
                 <x-slot name="content" class="">
