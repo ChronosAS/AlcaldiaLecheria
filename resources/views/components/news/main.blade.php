a6b34cb1e2575946a209b3b2c01a55a8713dcf78
leddinlozada223 <leddinlozada@outlook.com>
2024-07-25 12:42:45 -0400

    Alog

--- resources/views/components/news/main.blade.php
+++ resources/views/components/news/main.blade.php
@@ -4,82 +4,84 @@
             <div class="text-center py-8 space-y-3">
                 <h1 class="section-heading text-uppercase text-[3rem] text-white ">Noticias</h1>
             </div>
-            <div class=" flex-row sm:grid sm:grid-cols-10 sm:grid-rows-6 sm:gap-8 sm:mx-10 mb-10 ">
-                <div class="col-span-4 row-span-4 col-start-3">
-                    <div class="w-full h-full text-md sm:text-md ">
+            <div class=" sm:grid sm:grid-cols-7 sm:grid-rows-4 sm:gap-1 sm:mx-10 mb-10 ">
+                <div class="col-span-3 row-span-4 col-start-2">
+                    <div class="w-auto h-auto text-md sm:text-md ">
                          @livewire('news.news-carousel',['posts' => $this->carouselPosts])
                     </div>
                 </div>
-                @foreach ($sidePosts as $key => $post)
-                    <div class=" flex-row col-span-2 row-span-2 hidden m sm:inline-block max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-md
+               {{-- @foreach ($sidePosts as $key => $post)
+                    <div class=" flex-row row-span-2 h-auto col-start-5 hidden max-w-60 min-h-64 sm:inline-block bg-white rounded-lg shadow-md
                         @switch($key)
                             @case(5)
-                                 col-start-7
+                                row-span-2 col-start-6
                                 @break
                             @case(6)
-                                col-start-7 row-start-3
+                                row-span-2 col-start-5 row-start-3
                                 @break
                             @case(7)
-                                col-start-7 row-start-5
+                                row-span-2 col-start-6 row-start-3
                                 @break
-                            @case(8)
-                                col-start-5 row-start-5
-                                @break
-                            @case(9)
-                                col-start-3 row-start-5
-                                @break
-
                             @default
 
                         @endswitch">
-                        <a href="{{ route('app.news.show',$post->id) }}" class="relative">
-                                <img class="w-full h-56 object-cover" src="{{ $post->image }}" alt="{{ $post->imageAlt }}">
-                                <div class="grid justify-between  text-center">
-                                    <h5 class="text-xl font-semibold  ">{{ $post->title }}</h5>
+                        <div class="relative max-w-lg">
+                            <a href="{{ route('app.news.show',$post->id) }}" class="relative">
+                                <img class="w-full object-cover" src="{{ $post->image }}" alt="{{ $post->imageAlt }}">
+                                <div class="absolute flex truncate bg-slate-300">
+                                    <h5 class="text-xs font-bold tracking-tight text-black  truncate flex flex-col text-wrap  ">{{ $post->title }}</h5>
                                 </div>
                             </a>
+                        </div>
                     </div>
-                @endforeach
-                {{-- <div class=" flex-row col-span-2 row-span-2 col-start-7 hidden max-w-80 sm:inline-block">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
-                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=6&f=jpg&x=277&y=454&u=t" alt="">
-                        <div class="grid justify-between p-4 text-center">
-                            <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 ">Equipo de la Alcaldía de Lechería participó en el jurado de “Galería de Personajes” del colegio María Auxiliadora</h5>
-                        </div>
-                    </a>
+                @endforeach--}}
+                <div class="container col-span-2 row-span-4 col-start-5 flex-wrap">
+                    <div class="  h-auto hidden max-w-60 min-h-64 sm:inline-block">
+                        <a href="#">
+                            <div class="relative max-w-lg ">
+                                <img class=" w-full object-cover" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=4&f=jpg&x=277&y=454&u=t" alt="Random image">
+                            
+                                <div class="absolute flex truncate bg-slate-200">
+                                    <h5 class=" text-xs font-bold tracking-tight text-black  truncate flex flex-col text-wrap">Equipo de la Alcaldía de Lechería participó en el jurado de “Galería de Personajes” del colegio María Auxiliadora</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    
+                    <div class="   h-auto hidden max-w-60 min-h-64 sm:inline-block">
+                        <a href="#" class="rounded-lg">
+                            <div class="relative max-w-lg ">
+                                <img class=" w-full object-cover" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=4&f=jpg&x=277&y=454&u=t" alt="Random image">
+                                
+                                <div class="absolute flex truncate bg-slate-200">
+                                    <h5 class=" text-xs font-bold tracking-tight text-black  truncate flex flex-col text-wrap">Equipo de la Alcaldía de Lechería participó en el jurado de “Galería de Personajes” del colegio María Auxiliadora</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="   h-auto hidden max-w-60 min-h-64 sm:inline-block">
+                        <a href="#" class="rounded-lg">
+                            <div class="relative max-w-lg ">
+                                <img class=" w-full object-cover" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=4&f=jpg&x=277&y=454&u=t" alt="Random image">
+                                
+                                <div class="absolute flex truncate bg-slate-200">
+                                    <h5 class=" text-xs font-bold tracking-tight text-black  truncate flex flex-col text-wrap">Equipo de la Alcaldía de Lechería participó en el jurado de “Galería de Personajes” del colegio María Auxiliadora</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="  h-auto hidden max-w-60 min-h-64 sm:inline-block">
+                        <a href="#" class="rounded-lg">
+                            <div class="relative max-w-lg ">
+                                <img class=" w-full object-cover" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=4&f=jpg&x=277&y=454&u=t" alt="Random image">
+                                
+                               <div class="absolute flex truncate bg-slate-200">
+                                    <h5 class=" text-xs font-bold tracking-tight text-black  truncate flex flex-col text-wrap">Equipo de la Alcaldía de Lechería participó en el jurado de “Galería de Personajes” del colegio María Auxiliadora</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-                <div class=" flex-row col-span-2 row-span-2 col-start-7 row-start-3 gap-8 max-w-80 hidden sm:inline-block">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
-                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=4&f=jpg&x=277&y=454&u=t" alt="">
-                        <div class="grid justify-between p-4 text-center">
-                            <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 ">Equipo de Gestión Social realizó una ruta por tres colegios con actividades académicas</h5>
-                        </div>
-                    </a>
-                </div>
-                <div class=" flex-row col-span-2 row-span-2 col-start-7 row-start-5 gap-8 max-w-80 hidden sm:inline-block">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
-                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=4&f=jpg&x=277&y=454&u=t" alt="">
-                        <div class="grid justify-between p-4 text-center">
-                            <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 ">Equipo de Gestión Social realizó una ruta por tres colegios con actividades académicas</h5>
-                        </div>
-                    </a>
-                </div>
-                <div class=" flex-row col-span-2 row-span-2 col-start-5 row-start-5 gap-8 max-w-80 hidden sm:inline-block">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
-                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=4&f=jpg&x=277&y=454&u=t" alt="">
-                        <div class="grid justify-between p-4 text-center">
-                            <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 ">Alcalde Manuel Ferreira informó de los avances en la obra de embaulamiento del canal Rómulo Gallegos</h5>
-                        </div>
-                    </a>
-                </div>
-                <div class=" flex-row col-span-2 row-span-2 col-start-3 row-start-5  gap-8 max-w-80 hidden sm:inline-block">
-                    <a href="#" class=" items-center bg-white border border-gray-200 rounded-lg shadow md:grid md:max-w-xl lg:size-[340px] hover:bg-gray-100 ">
-                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-full md:rounded-b-none" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1npmiU.img?w=300&h=225&q=90&m=4&f=jpg&x=277&y=454&u=t" alt="">
-                        <div class="grid justify-between p-4 text-center">
-                            <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 ">Programa "Abuelos de Lechería" de Gestión Social continúa aportando bienestar general a adultos mayores</h5>
-                        </div>
-                    </a>
-                </div> --}}
             </div>
         </div>
     </div>
