30822a10bd8c3a7593b1ea3c5056e293002829ea
leddinlozada223 <leddinlozada@outlook.com>
2024-08-27 13:38:48 -0400

    vdv header

--- resources/views/vdv.blade.php
+++ resources/views/vdv.blade.php
@@ -1,7 +1,28 @@
 <x-main-layout class="dark:bg-slate-300">
     <div>
-        <x-main-header bg_img="../assets/img/lecheria-bg.jpg">
-            <h1 class="text-7xl">VDV2024</h1>
+        <x-main-header bg_img="../assets/img/VDV/banner.webp">
+            <img class="sm:h-80" src="{{ asset('../assets/img/VDV/logo-vdv.webp') }}">
         </x-main-header>
     </div>
+    {{--<x-full-card>
+        <x-slot name="content">
+            <div class="p-5 sm:p-8">
+                <div class="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01493.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01496.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01498.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01500.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01501.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01504.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01506.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01515.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01523.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01529.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01534.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01539.webp')}}">
+                    <img src="{{asset('../assets/img/VDV/VDVGallery/DSC01545.webp')}}">
+                </div>
+            </div>
+        </x-slot>
+    </x-full-card>--}}
 </x-main-layout>
