fe61374f9c9f0d5f1b47d7dcf34aa10997407e02
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2024-08-29 14:32:37 -0400

    position change mobile

--- resources/views/vdv.blade.php
+++ resources/views/vdv.blade.php
@@ -1,6 +1,6 @@
 <x-main-layout class="dark:bg-slate-300">
     <div>
-        <x-main-header class="sm:bg-[0 55%]" bg_img="../assets/img/VDV/banner-vdv.webp">
+        <x-main-header class="sm:bg-[0 55%] sm:bg-cover bg-[length:411px_747px]" bg_img="../assets/img/VDV/banner-vdv.webp">
             <img class="sm:h-80" src="{{ asset('../assets/img/VDV/logo-vdv.webp') }}">
         </x-main-header>
     </div>
