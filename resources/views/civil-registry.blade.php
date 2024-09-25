33ea54a546f2e53ad4d6fa4ee5e1f6969674b518
leddinlozada223 <leddinlozada@outlook.com>
2024-09-25 10:50:10 -0400

    Civil register tab-cola

--- resources/views/civil-registry.blade.php
+++ resources/views/civil-registry.blade.php
@@ -1,32 +1,60 @@
-<x-main-layout class="dark:bg-slate-300">
+<x-main-layout class="bg-slate-300">
     <div x-data="{ title: 'Registro Civil' }">
         <x-main-header bg_img="../assets/img/lecheria-bg.jpg">
             <h1 class="text-7xl" x-text='title' ></h1>
         </x-main-header>
-        <div x-data="{ tab: 'Registro Civil'}" class="mt-4 px-4 mx-auto max-w-7xl border-b border-gray-700 bg-blue-900 sm:rounded-xl">
+        <div x-data="{ tab: 'Registro Civil'}" class=" mx-auto max-w-7xl ">
             <ul
-                class="flex flex-wrap -mb-px text-lg font-medium text-center items-center justify-evenly text-white shadow-2xl"
+                class="flex flex-wrap -mb-px  font-medium text-center items-center gap-y-4 sm:justify-between justify-evenly"
                 id="civil-registry-tab"
                 data-tabs-toggle="#civil-registry-tab-content"
                 role="tablist"
-                data-tabs-active-classes="text-white hover:text-blue-500 dark:text-white dark:hover:text-blue-400 border-blue-500 dark:border-blue-400"
-                data-tabs-inactive-classes="border-transparent text-white hover:text-blue-500 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300">
-                {{-- <li class="me-2" role="presentation">
-                    <button @click="title = 'Historia'"  class="inline-block  p-4 border-b-2 rounded-t-lg dark:text-white" id="history-tab" data-tabs-target="#history" type="button" role="tab" aria-controls="history" aria-selected="false">Historia</button>
+                data-tabs-active-classes="hover:border-blue-500 hover:border-blue-400 border-blue-700"
+                data-tabs-inactive-classes="border-transparent hover:border-blue-500 hover:border-blue-300 border-blue-700 hover:border-blue-300">
+                <li class="pb-0 p-2 hover:border-blue-900  border-transparent border-4" role="presentation">
+                    <a @click="title = 'Historia'"  class="inline-block  " id="history-tab" data-tabs-target="#history"  role="tab" aria-controls="history" aria-selected="false">   
+                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/conducta.webp')}}"/>
+                    </a>
                 </li>
-                <li class="me-2" role="presentation">
-                    <button @click="title = 'Himno'" class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-300 hover:border-gray-300 dark:hover:text-gray-300 dark:text-white" id="hymn-tab" data-tabs-target="#hymn" type="button" role="tab" aria-controls="hymn" aria-selected="false">Himno</button>
+                <li class="pb-0 p-2  hover:border-blue-900  border-transparent border-4" role="presentation">
+                    <a @click="title = 'Himno'" class="inline-block " id="hymn-tab" data-tabs-target="#hymn"  role="tab" aria-controls="hymn" aria-selected="false">
+                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/fe-de-vida.webp')}}"/>
+                    </a>
                 </li>
-                <li class="me-2" role="presentation">
-                    <button @click="title = 'Escudo'" class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-300 hover:border-gray-300 dark:hover:text-gray-300 dark:text-white" id="shield-tab" data-tabs-target="#shield" type="button" role="tab" aria-controls="shield" aria-selected="false">Escudo</button>
+                <li class="pb-0 p-2  hover:border-blue-900  border-transparent border-4" role="presentation">
+                    <a @click="title = 'Escudo'" class="inline-block " id="shield-tab" data-tabs-target="#shield"  role="tab" aria-controls="shield" aria-selected="false">
+                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/matrimonios-70.webp')}}"/>
+                    </a>
                 </li>
-                <li role="presentation">
-                    <button @click="title = 'Bandera'" class="inline-block  p-4 border-b-2 rounded-t-lg hover:text-gray-300 hover:border-gray-300 dark:hover:text-gray-300 dark:text-white" id="flag-tab" data-tabs-target="#flag" type="button" role="tab" aria-controls="flag" aria-selected="false">Bandera</button>
-                </li> --}}
+                <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" role="presentation">
+                    <a @click="title = 'Bandera'" class="inline-block  " id="flag-tab" data-tabs-target="#flag"  role="tab" aria-controls="flag" aria-selected="false">
+                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/matrimonios.webp')}}"/>
+                    </a>
+                </li> 
+                 <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" role="presentation">
+                    <a @click="title = 'Bandera'" class="inline-block  " id="flag-tab" data-tabs-target="#flag"  role="tab" aria-controls="flag" aria-selected="false">
+                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/mudanza.webp')}}"/>
+                    </a>
+                </li> 
+                 <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" role="presentation">
+                    <a @click="title = 'Bandera'" class="inline-block  " id="flag-tab" data-tabs-target="#flag"  role="tab" aria-controls="flag" aria-selected="false">
+                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/nac.webp')}}"/>
+                    </a>
+                </li> 
+                 <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" role="presentation">
+                    <a @click="title = 'Bandera'" class="inline-block  " id="flag-tab" data-tabs-target="#flag"  role="tab" aria-controls="flag" aria-selected="false">
+                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/residencia.webp')}}"/>
+                    </a>
+                </li> 
+                 <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" role="presentation">
+                    <a @click="title = 'Bandera'" class="inline-block  " id="flag-tab" data-tabs-target="#flag"  role="tab" aria-controls="flag" aria-selected="false">
+                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/solteria.webp')}}"/>
+                    </a>
+                </li> 
             </ul>
         </div>
         <div id="civil-registry-tab-content">
-            {{-- <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-slate-300" id="history" role="tabpanel" aria-labelledby="history-tab">
+            <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-slate-300" id="history" role="tabpanel" aria-labelledby="history-tab">
                 <x-info.history/>
             </div>
             <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-slate-300" id="hymn" role="tabpanel" aria-labelledby="hymn-tab">
@@ -37,7 +65,7 @@
             </div>
             <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-slate-300" id="flag" role="tabpanel" aria-labelledby="flag-tab">
                 <x-info.flag/>
-            </div> --}}
+            </div>
         </div>
     </div>
 </x-main-layout>
