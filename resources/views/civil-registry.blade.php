f063e881309508041197ec3e3677028708d7ac92
leddinlozada223 <leddinlozada@outlook.com>
2024-09-26 10:34:03 -0400

    basic title

--- resources/views/civil-registry.blade.php
+++ resources/views/civil-registry.blade.php
@@ -8,42 +8,42 @@
                 class="flex flex-wrap -mb-px  font-medium text-center items-center gap-y-4 sm:justify-between justify-evenly"
                 id="civil-registry-tab">
                 <li class="pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a   href="" class="inline-block cursor-pointer  " id="conduct-tab" >   
+                    <a   href="{{ route('') }}" class="inline-block cursor-pointer  " id="conduct-tab" >   
                         <img class="sm:min-w-[17rem]  sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/conducta.webp')}}"/>
                     </a>
                 </li>
                 <li class="pb-0 p-2  hover:border-blue-900  border-transparent border-4" >
-                    <a  href="" class="inline-block cursor-pointer ">
+                    <a  href="{{ route('') }}" class="inline-block cursor-pointer ">
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/fe-de-vida.webp')}}"/>
                     </a>
                 </li>
                 <li class="pb-0 p-2  hover:border-blue-900  border-transparent border-4" >
-                    <a  href="" class="inline-block cursor-pointer " >
+                    <a  href="{{ route('') }}" class="inline-block cursor-pointer " >
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/matrimonios-70.webp')}}"/>
                     </a>
                 </li>
                 <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a  href="" class="inline-block cursor-pointer  ">
+                    <a  href="{{ route('') }}" class="inline-block cursor-pointer  ">
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/matrimonios.webp')}}"/>
                     </a>
                 </li> 
                  <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a href="" class="inline-block cursor-pointer  " >
+                    <a href="{{ route('') }}" class="inline-block cursor-pointer  " >
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/mudanza.webp')}}"/>
                     </a>
                 </li> 
                  <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a href="" class="inline-block cursor-pointer  " >
+                    <a href="{{ route('') }}" class="inline-block cursor-pointer  " >
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/nac.webp')}}"/>
                     </a>
                 </li> 
                  <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a href="" class="inline-block cursor-pointer  " >
+                    <a href="{{ route('') }}" class="inline-block cursor-pointer  " >
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/residencia.webp')}}"/>
                     </a>
                 </li> 
                  <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a href="" class="inline-block cursor-pointer  " >
+                    <a href="{{ route('') }}" class="inline-block cursor-pointer  " >
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/solteria.webp')}}"/>
                     </a>
                 </li> 
---
+++ resources/views/civil-registry/matrimonio-70.blade.php
@@ -0,0 +1,7 @@
+<x-main-layout class="bg-slate-300">
+    <div x-data="{ title: 'Matrimonios Art-70' }">
+        <x-main-header bg_img="../assets/img/lecheria-bg.jpg">
+            <h1 class="text-7xl" x-text='title' ></h1>
+        </x-main-header>
+    </div>
+</x-main-layout>
