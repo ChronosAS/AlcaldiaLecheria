592a4607f28a8c546044be59c1592a59f0461138
leddinlozada223 <leddinlozada@outlook.com>
2024-09-27 13:39:25 -0400

    defunciones y union estable

--- resources/views/civil-registry.blade.php
+++ resources/views/civil-registry.blade.php
@@ -27,21 +27,31 @@
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/mudanza.webp')}}"/>
                     </a>
                 </li>
-                 <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
+                <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
                     <a href="{{ route('app.civil-reg.nacimientos') }}" class="inline-block cursor-pointer  " >
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/nac.webp')}}"/>
                     </a>
                 </li>
-                 <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
+                <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
                     <a href="{{ route('app.civil-reg.residencia') }}" class="inline-block cursor-pointer  " >
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/residencia.webp')}}"/>
                     </a>
                 </li>
-                 <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
+                <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
                     <a href="{{ route('app.civil-reg.solteria') }}" class="inline-block cursor-pointer  " >
                         <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/solteria.webp')}}"/>
                     </a>
                 </li>
+                {{-- <li class="pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
+                    <a   href="{{ route('app.civil-reg.conducta') }}" class="inline-block cursor-pointer  " id="conduct-tab" >
+                        <img class="sm:min-w-[17rem]  sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/uniones.webp')}}"/>
+                    </a>
+                </li>
+                <li class="pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
+                    <a   href="{{ route('app.civil-reg.conducta') }}" class="inline-block cursor-pointer  " id="conduct-tab" >
+                        <img class="sm:min-w-[17rem]  sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/defunciones.webp')}}"/>
+                    </a>
+                </li> --}}
             </ul>
             {{-- <h1 class="text-center text-3xl font-bold pt-16">Todas las citas y solicitudes de tramites se realizaran <br> de Lunes a Jueves de 8:00am a 11:30am</h1> --}}
         </div>
---
+++ resources/views/civil-registry/defuncion.blade.php
@@ -0,0 +1,53 @@
+<x-main-layout class="bg-slate-300">
+    <div x-data="{ title: 'Defunciones' }">
+        <x-main-header bg_img="../assets/img/lecheria-bg.jpg">
+            <h1 class="text-7xl" x-text='title' ></h1>
+        </x-main-header>
+        <div class="my-8 mt-0 space-y-4 px-4 mx-auto max-w-5xl">
+            <x-full-card>
+                <x-slot name="title">
+                    <div class=" text-3xl">
+       
+                    </div>
+                </x-slot>
+                <x-slot name="subTitle">
+                    <div class="text-xl">
+                        <div class="flex items-center justify-center">
+                        <img src="{{asset('assets/img/civil-registry/registry-info/defuncion.webp')}}" class="min-h-[15rem] max-h-[18rem] min-w-[30rem] max-w-[35rem] m-4 transition duration-300 ease-in-out hover:shadow-lg rounded-lg">
+                        </div>
+                    </div>
+                </x-slot>
+                <x-slot name="content" class="center flex flex-col">
+                    <div x-data="{ selectedTab: 'requirements' }" class="max-w-xl mt-4 px-4 mx-auto items-center">
+                        <div @keydown.right.prevent="$focus.wrap().next()" @keydown.left.prevent="$focus.wrap().previous()" class="flex gap-2 overflow-x-auto border-b border-slate-300 justify-evenly " role="tablist" aria-label="tab options">
+                            <button @click="selectedTab = 'requirements'" :aria-selected="selectedTab === 'requirements'" :tabindex="selectedTab === 'requirements' ? '0' : '-1'" :class="selectedTab === 'requirements' ? 'font-bold text-blue-600 border-b-2 border-blue-700  ' : 'text-black font-medium    hover:border-b-2 hover:border-b-blue-400 hover:text-black'" class="h-min px-4 py-2 text-md" type="button" role="tab" aria-controls="tabpanelRequirements" >Requisitos</button>
+                            <button @click="selectedTab = 'info'" :aria-selected="selectedTab === 'info'" :tabindex="selectedTab === 'info' ? '0' : '-1'" :class="selectedTab === 'info' ? 'font-bold text-blue-600 border-b-2 border-blue-700  ' : 'text-black font-medium    hover:border-b-2 hover:border-b-blue-400 hover:text-black'" class="h-min px-4 py-2 text-md" type="button" role="tab" aria-controls="tabpanelInfo" >Información importante</button>
+                        </div>
+                        <div class="px-2 py-4 text-black ">
+                            <div x-show="selectedTab === 'requirements'" id="tabpanelRequirements" role="tabpanel" aria-label="requirements" class="text-center">
+                                <ul class="max-w-xl text-md font-medium  text-black bg-white border border-blue-200 rounded-lg">
+                                    <li class="w-full px-4 py-2 border-b border-blue-300 rounded-t-lg ">1.- Original y 3 copias de la cédula de identidad del exponente, el cual debe ser un familiar directo (Padres, hijos o cónyuge).</li>
+                                    <li class="w-full px-4 py-2 border-b border-blue-300 ">2.- Original y cuatro (04) Copia del Certificado de defunción expedido por la entidad hospitalaria o clínica (debidamente firmada y sellado). </li>
+                                    <li class="w-full px-4 py-2 border-b border-blue-300 ">3.- Cuatro (04) Copias de la Cédula de Identidad del difunto(a).</li>
+                                    <li class="w-full px-4 py-2 border-b border-blue-300 rounded-t-lg ">4.- Copia de la cédula de identidad de todos los hijos legítimos y reconocidos.</li>
+                                    <li class="w-full px-4 py-2 border-b border-blue-300 rounded-t-lg ">5.- Copia de la Cédula de Identidad de (2) dos testigos mayores de 18 años los cuales deberán estar presentes al momento del asentamiento. (obligatorio).</li>
+
+                                </ul>
+                            </div>
+                            <div x-show="selectedTab === 'info'" id="tabpanelInfo" role="tabpanel" aria-label="info" class="text-center">
+                                <ul class="max-w-xl text-lg  font-black  text-black bg-white border border-blue-200 rounded-lg ">
+                                    <li class="w-full px-4 py-2 border-b border-blue-300 rounded-t-lg ">- Las Defunciones se realizan en el Registro Civil en la Av. Anzoátegui con Calle Los Almendrones, Quinta Araguaney, Diagonal a la Plaza “El Parque”.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex  flex-row items-center justify-center space-x-5 ">
+                        <a  href="{{ route('app.civil-registry') }}" class=" cursor-pointer whitespace-nowrap rounded-xl bg-blue-700 px-4 py-2 text-lg font-medium tracking-wide text-slate-100 transition hover:opacity-75 text-center focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 active:opacity-100 active:outline-offset-0 disabled:opacity-75 disabled:cursor-not-allowed ">Regresar</a>
+                        
+                        <a  href="" class=" cursor-pointer whitespace-nowrap rounded-xl bg-green-700 px-4 py-2 text-lg font-medium tracking-wide text-slate-100 transition hover:opacity-75 text-center focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700 active:opacity-100 active:outline-offset-0 disabled:opacity-75 disabled:cursor-not-allowed  ">Descargar Formulario</a>
+                    </div>
+                </x-slot>
+            </x-full-card>
+        </div>
+    </div>
+</x-main-layout>
