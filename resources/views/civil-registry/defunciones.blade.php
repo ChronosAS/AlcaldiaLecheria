89855f514a7c5a3fec11d493f19b0ced6b9459d9
leddinlozada223 <leddinlozada@outlook.com>
2024-09-30 11:41:23 -0400

    numero de finde semanas

--- resources/views/civil-registry/defunciones.blade.php
+++ resources/views/civil-registry/defunciones.blade.php
@@ -17,6 +17,7 @@
                     <div x-data="{ selectedTab: 'requirements' }" class="max-w-xl mt-4 px-4 mx-auto items-center">
                         <div @keydown.right.prevent="$focus.wrap().next()" @keydown.left.prevent="$focus.wrap().previous()" class="flex gap-2 overflow-x-auto border-b border-slate-300 justify-evenly " role="tablist" aria-label="tab options">
                             <button @click="selectedTab = 'requirements'" :aria-selected="selectedTab === 'requirements'" :tabindex="selectedTab === 'requirements' ? '0' : '-1'" :class="selectedTab === 'requirements' ? 'font-bold text-blue-600 border-b-2 border-blue-700  ' : 'text-black font-medium    hover:border-b-2 hover:border-b-blue-400 hover:text-black'" class="h-min px-4 py-2 text-md" type="button" role="tab" aria-controls="tabpanelRequirements" >Requisitos</button>
+                            <button @click="selectedTab = 'Weekend'" :aria-selected="selectedTab === 'Weekend'" :tabindex="selectedTab === 'Weekend' ? '0' : '-1'" :class="selectedTab === 'weekend' ? 'font-bold text-blue-700 border-b-2 border-blue-700  ' : 'text-slate-700 font-medium    hover:border-b-2 hover:border-b-slate-800 hover:text-black'" class="h-min px-4 py-2 text-md" type="button" role="tab" aria-controls="tabpaneWeekend" >Defunciones de fines de semana</button>
                             <button @click="selectedTab = 'info'" :aria-selected="selectedTab === 'info'" :tabindex="selectedTab === 'info' ? '0' : '-1'" :class="selectedTab === 'info' ? 'font-bold text-blue-600 border-b-2 border-blue-700  ' : 'text-black font-medium    hover:border-b-2 hover:border-b-blue-400 hover:text-black'" class="h-min px-4 py-2 text-md" type="button" role="tab" aria-controls="tabpanelInfo" >Información importante</button>
                         </div>
                         <div class="px-2 py-4 text-black ">
@@ -28,6 +29,13 @@
                                     <li class="w-full px-4 py-2 border-b border-blue-300 rounded-t-lg ">4.- Copia de la cédula de identidad de todos los hijos legítimos y reconocidos.</li>
                                     <li class="w-full px-4 py-2 border-b border-blue-300 rounded-t-lg ">5.- Copia de la Cédula de Identidad de (2) dos testigos mayores de 18 años los cuales deberán estar presentes al momento del asentamiento. (obligatorio).</li>
 
+                                </ul>
+                            </div>
+                            <div x-show="selectedTab === 'weekend'" id="tabpaneWeekend" role="tabpanel" aria-label="weekend" class="text-center">
+                                <ul class="max-w-3xl text-md font-medium  text-black bg-white border border-blue-200 rounded-lg">
+                                    <li class="w-full px-4 py-2 border-b border-blue-300 rounded-t-lg "> Solo para defunciones los fines de Semana y Feriados  </li>
+                                    <li class="w-full px-4 py-2 border-b border-blue-300 rounded-t-lg "> Teléfono de Guardia </li>
+                                    <li class="w-full px-4 py-2 border-b border-blue-300 rounded-t-lg "> 04162830854 </li>
                                 </ul>
                             </div>
                             <div x-show="selectedTab === 'info'" id="tabpanelInfo" role="tabpanel" aria-label="info" class="text-center">
