53b9e78a0651f90f5cdb6b7eec5a65133fe99194
leddinlozada223 <leddinlozada@outlook.com>
2024-11-01 10:26:50 -0400

    border gone, animation lets go

--- resources/views/civil-registry.blade.php
+++ resources/views/civil-registry.blade.php
@@ -7,44 +7,44 @@
             <ul
                 class="flex flex-wrap -mb-px  font-medium text-center items-center gap-y-4 sm:justify-evenly justify-evenly xl:pl-10 xl:pr-10 2xl:pl-5 2xl:pr-5 py-10"
                 id="civil-registry-tab">
-                <li class="pb-0 p-2  hover:border-blue-900  border-transparent border-4" >
-                    <a  href="{{ route('app.civil-reg.fe-de-vida') }}" class="inline-block cursor-pointer ">
-                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/fe-de-vida.webp')}}"/>
+                <li class="pb-0 p-2    border-transparent border-4 rounded-lg" >
+                    <a  href="{{ route('app.civil-reg.fe-de-vida') }}" class="inline-block cursor-pointer hover:-translate-y-1 hover:scale-110 ease-in-out duration-300 transition">
+                        <img class=" rounded-lg sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/fe-de-vida.webp')}}"/>
                     </a>
                 </li>
-                <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a  href="{{ route('app.civil-reg.matri') }}" class="inline-block cursor-pointer  ">
-                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/matrimonios.webp')}}"/>
+                <li class=" pb-0 p-2   border-transparent border-4 rounded-lg" >
+                    <a  href="{{ route('app.civil-reg.matri') }}" class="inline-block cursor-pointer hover:-translate-y-1 hover:scale-110 ease-in-out duration-300 transition">
+                        <img class=" rounded-lg sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/matrimonios.webp')}}"/>
                     </a>
                 </li>
-                <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a href="{{ route('app.civil-reg.mudanza') }}" class="inline-block cursor-pointer  " >
-                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/mudanza.webp')}}"/>
+                <li class=" pb-0 p-2   border-transparent border-4 rounded-lg" >
+                    <a href="{{ route('app.civil-reg.mudanza') }}" class="inline-block cursor-pointer hover:-translate-y-1 hover:scale-110 ease-in-out duration-300 transition " >
+                        <img class=" rounded-lg sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/mudanza.webp')}}"/>
                     </a>
                 </li>
-                <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a href="{{ route('app.civil-reg.nacimientos') }}" class="inline-block cursor-pointer  " >
-                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/nac.webp')}}"/>
+                <li class=" pb-0 p-2   border-transparent border-4 rounded-lg" >
+                    <a href="{{ route('app.civil-reg.nacimientos') }}" class="inline-block cursor-pointer hover:-translate-y-1 hover:scale-110 ease-in-out duration-300 transition" >
+                        <img class=" rounded-lg sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/nac.webp')}}"/>
                     </a>
                 </li>
-                <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a href="{{ route('app.civil-reg.residencia') }}" class="inline-block cursor-pointer  " >
-                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/residencia.webp')}}"/>
+                <li class=" pb-0 p-2   border-transparent border-4 rounded-lg" >
+                    <a href="{{ route('app.civil-reg.residencia') }}" class="inline-block cursor-pointer hover:-translate-y-1 hover:scale-110 ease-in-out duration-300 transition" >
+                        <img class=" rounded-lg sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/residencia.webp')}}"/>
                     </a>
                 </li>
-                <li class=" pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a href="{{ route('app.civil-reg.solteria') }}" class="inline-block cursor-pointer  " >
-                        <img class="sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/solteria.webp')}}"/>
+                <li class=" pb-0 p-2   border-transparent border-4 rounded-lg" >
+                    <a href="{{ route('app.civil-reg.solteria') }}" class="inline-block cursor-pointer hover:-translate-y-1 hover:scale-110 ease-in-out duration-300 transition" >
+                        <img class=" rounded-lg sm:min-w-[17rem] sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/solteria.webp')}}"/>
                     </a>
                 </li>
-                <li class="pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a   href="{{ route('app.civil-reg.union') }}" class="inline-block cursor-pointer  ">
-                        <img class="sm:min-w-[17rem]  sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/union.webp')}}"/>
+                <li class="pb-0 p-2   border-transparent border-4 rounded-lg" >
+                    <a   href="{{ route('app.civil-reg.union') }}" class="inline-block cursor-pointer hover:-translate-y-1 hover:scale-110 ease-in-out duration-300 transition">
+                        <img class=" rounded-lg sm:min-w-[17rem]  sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{asset('assets/img/civil-registry/union.webp')}}"/>
                     </a>
                 </li>
-                <li class="pb-0 p-2 hover:border-blue-900  border-transparent border-4" >
-                    <a   href="{{ route('app.civil-reg.defunciones') }}" class="inline-block cursor-pointer  ">
-                        <img class="sm:min-w-[17rem]  sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{ asset('assets/img/civil-registry/defunciones.webp') }}"/>
+                <li class="pb-0 p-2   border-transparent border-4 rounded-lg " >
+                    <a   href="{{ route('app.civil-reg.defunciones') }}" class="inline-block cursor-pointer  hover:-translate-y-1 hover:scale-110 ease-in-out duration-300 transition">
+                        <img class=" rounded-lg sm:min-w-[17rem]  sm:min-h-[13rem] sm:max-w-[17rem] sm:max-h-[13rem] min-w-[7rem] min-h-[3rem] max-w-[9rem] max-h-[6rem]" src="{{ asset('assets/img/civil-registry/defunciones.webp') }}"/>
                     </a>
                 </li>
             </ul>
