88f9a4b4aac38041c5d8e6c336059f8700368f5d
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2025-09-22 14:58:10 -0400

    Implement rate limiting in contact form submission and update dependencies

--- app/Livewire/ContactForm.php
+++ app/Livewire/ContactForm.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
-use App\Mail\CitizenService;
+use App\Mail\ContactUsMail;
 use Illuminate\Support\Facades\Mail;
+use DanHarrin\LivewireRateLimiting\WithRateLimiting;
 use Livewire\Component;
 
 class ContactForm extends Component
 {
+    use WithRateLimiting;
+
     public $fullName;
     public $document;
     public $email;
@@ -18,9 +21,14 @@ class ContactForm extends Component
     public function send()
     {
         //  $this->validate([]);
+        try {
+            $this->rateLimit(3);
+        } catch (\DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException $e) {
+            $this->addError('rateLimit', 'Demasiados intentos. Por favor, inténtelo de nuevo más tarde.');
+            return;
+        }
 
-
-        // Mail::to($this->email)->send(new ContactUsMail());
+        Mail::to('ale.aroutin@gmail.com')->send(new ContactUsMail());
     }
 
     public function render()
