561f0b96a333094d662433d407cdaef4cc0fa0de
leddinlozada223 <leddinlozada@outlook.com>
2025-09-30 11:31:13 -0400

    Update contact email template colors for improved readability

--- resources/views/mail/contact-us-mail.blade.php
+++ resources/views/mail/contact-us-mail.blade.php
@@ -2,11 +2,11 @@
 # <span>Ha recibido una Solicitud de Atención al Ciudadano!</span>
 
 <x-mail::panel>
-<strong>Nombre: </strong><span style="color:#374151;">{{ $firstname }}</span><strong>Apellido: </strong><span style="color:#374151;">{{ $lastname }}</span><br>
-<strong>Correo: </strong><span style="color:#374151;">{{ $email }}</span><br>
-<strong>Cedula: </strong><span style="color:#374151;">{{ $id }}</span><br>
-<strong>Número de Telefono: </strong><span style="color:#374151;">{{ $phone }}</span><br>
-<strong>Departamento: </strong><span style="color:#374151;">{{ $department }}</span><br>
+<strong>Nombre: </strong><span style="color:###1a1a1a;">{{ $firstname }}</span><strong>Apellido: </strong><span style="color:###1a1a1a;">{{ $lastname }}</span><br>
+<strong>Correo: </strong><span style="color:###1a1a1a;">{{ $email }}</span><br>
+<strong>Cedula: </strong><span style="color:###1a1a1a;">{{ $id }}</span><br>
+<strong>Número de Telefono: </strong><span style="color:###1a1a1a;">{{ $phone }}</span><br>
+<strong>Departamento: </strong><span style="color:###1a1a1a;">{{ $department }}</span><br>
 </x-mail::panel>
-<strong>Solicitud: </strong><span style="color:#374151;">{{ $application }}</span><br>
+<strong>Solicitud: </strong><span style="color:###1a1a1a;">{{ $application }}</span><br>
 </x-mail::message>
