12c11717e70c9814a8246ba58ac3f455ce70652e
leddinlozada223 <leddinlozada@outlook.com>
2026-01-06 11:28:36 -0400

    refactor: update button and section styles for improved dark mode consistency

--- resources/views/components/table/table-rounded.blade.php
+++ resources/views/components/table/table-rounded.blade.php
@@ -1,16 +1,16 @@
 <div class="relative overflow-x-auto sm:overflow-x-hidden shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead {{ $attributes->merge(['class'=>'text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400']) }}>
+    <table class="w-full text-sm text-left rtl:text-right text-gray-400">
+        <thead {{ $attributes->merge(['class'=>'text-xs  uppercase bg-gray-700 text-gray-400']) }}>
             {{ $thead }}
         </thead>
         <tbody {{ $attributes }}>
             {{ $tbody }}
-            {{-- <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            {{-- <tr class="border-b bg-gray-800 border-gray-700">
             </tr> --}}
         </tbody>
         {{-- <tfoot>
             {{ $tfoot }}
-            {{-- <tr class="font-semibold text-gray-900 dark:text-white">
+            {{-- <tr class="font-semibold text-white">
                 <th scope="row" class="px-6 py-3 text-base">Total</th>
                 <td class="px-6 py-3">3</td>
                 <td class="px-6 py-3">21,000</td>
--- resources/views/components/table/table.blade.php
+++ resources/views/components/table/table.blade.php
@@ -1,16 +1,16 @@
 <div class="relative overflow-x-auto shadow-md">
-    <table class="w-full border-white text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <table class="w-full border-white text-sm text-left rtl:text-right text-gray-400">
         <thead {{ $attributes->merge(['text-xs text-white-700 uppercase bg-gray-50 ']) }}>
             {{ $thead }}
         </thead>
         <tbody>
             {{ $tbody }}
-            {{-- <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            {{-- <tr class=" border-b bg-gray-800 border-gray-700">
             </tr> --}}
         </tbody>
         {{-- <tfoot>
             {{ $tfoot }}
-            {{-- <tr class="font-semibold text-gray-900 dark:text-white">
+            {{-- <tr class="font-semibold text-white">
                 <th scope="row" class="px-6 py-3 text-base">Total</th>
                 <td class="px-6 py-3">3</td>
                 <td class="px-6 py-3">21,000</td>
